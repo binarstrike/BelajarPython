e9cb89fa3cd27bbe9d6a26152393754afa04a863
Binar100804 <binarnugroho775@gmail.com>
2022-07-06 19:38:33 +0700

    update

--- Latihan/Kalkulator/kalkulator.py
+++
@@ -1,7 +0,0 @@
-def hitung(x,opr,y):
-	if opr == '+':
-		return x + y
-	if opr == '-':
-		return x - y
-		
-print(hitung(5,'+',6))
--- Latihan/OpenFile/open_file.py
+++
@@ -1,9 +0,0 @@
-def main():
-	file = open('file.txt','w+')
-	file.write('Hello, world!!')
-	file.close()
-	file = open('file.txt', 'r')
-	print(file.read())
-	file.close()
-if __name__ == '__main__':
-	main()
