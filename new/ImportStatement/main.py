0d39ce07c564267d767a4f997a2fc2b627d19333
Binar100804 <binarnugroho775@gmail.com>
2022-07-06 19:59:52 +0700

    update

--- new/ImportStatement/main.py
+++ new/ImportStatement/main.py
@@ -1,7 +1,7 @@
 import variabel
 import matematika
 from variabel import hobi
-from matematika import kurang, bagi
+from matematika import kurang, bagi, luas_lingkaran
 
 hasil = matematika.tambah(5, 5)
 print(hasil)
@@ -10,5 +10,7 @@ hasil = kurang(20, 15)
 print(hasil)
 
 print(bagi(100, 25))
+
+print(luas_lingkaran(10))
 
 print("halo nama saya {0} \nhobi saya {1}".format(variabel.nama, hobi))
