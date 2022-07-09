7b7b297b4350cc3432383472830e64bd588faae0
Binar100804 <binarnugroho775@gmail.com>
2022-07-09 08:26:24 +0700

    update dari test branch

---
+++ new/UserInput/main.py
@@ -0,0 +1,8 @@
+# input()
+#							↓	type data, jika tidak ada maka user bisa memasukkan input dengan type data apa saja
+ex_string_input = str(input("masukkan sebuah teks berupa string : "))
+# menampilkan input data dari user
+print(ex_string_input)
+
+ex_int_input = int(input("masukkan angka : "))
+print(ex_int_input)
