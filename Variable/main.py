24d7e0c62eafea1a7e2c67ef31959a0188ad7938
Binar100804 <binarnugroho775@gmail.com>
2022-07-09 13:37:34 +0700

    update -> Variabel

---
+++ Variable/main.py
@@ -0,0 +1,7 @@
+# deklarasi variabel pada python
+# nama_variabel = nilai/data yang disimpan pada variabel dapat berupa string dan integer/angka
+
+nama = "binar nugroho"
+umur = 17
+
+print(f"nama saya {nama}\numur {umur}")
