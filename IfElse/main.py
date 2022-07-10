a3f05870ba651d911372b2aaff55a290ebcc7e9b
Binar100804 <binarnugroho775@gmail.com>
2022-07-10 13:37:12 +0000

    update

---
+++ IfElse/main.py
@@ -0,0 +1,15 @@
+"""
+if kondisi:
+	perintah yang akan dijalankan
+
+if kondisi:
+	perintah yang akan dijalankan
+else:
+	perintah yang akan dijalankan
+
+if kondisi:
+	perintah yang akan dijalankan
+elif perintah yang akan dijalankan:
+	perintah yang akan dijalankan
+
+"""
